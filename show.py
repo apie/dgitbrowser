e5eabd0586dc1d8cd45caed694b6b6d12ed2abc7
Denick <1049485+apie@users.noreply.github.com>
2025-11-09 17:52:29 +0100

    pass "repo" parameter + add function to show readme file

--- show.py
+++ show.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 import sys
 from datetime import datetime, timezone, timedelta
+from pathlib import Path
 
 import pygit2
 from pygments import highlight
-from pygments.lexers import diff
+from pygments.lexers import diff, markup
 from pygments.formatters import terminal
 
+import logging
 
-def show_stat(commit):
+logger = logging.getLogger(__name__)
+
+
+def show_readme(repo_path: str):
+    p = Path(repo_path)
+    readme = p / "README.md"
+    print("Readme:")
+    if not readme.exists():
+        return logger.warning("No README.md found")
+
+    print(
+        highlight(
+            readme.read_text(), markup.MarkdownLexer(), terminal.TerminalFormatter()
+        )
+    )
+
+
+def show_stat(repo, commit):
     if commit.parents:
         rdiff = repo.diff(commit.parents[0], commit)
     else:
@@ -20,7 +39,7 @@ def show_stat(commit):
     print(highlight(rdiff.patch, diff.DiffLexer(), terminal.TerminalFormatter()))
 
 
-def git_show(commit):
+def git_show(repo, commit):
     tzinfo = timezone(timedelta(minutes=commit.author.offset))
 
     dt = datetime.fromtimestamp(float(commit.author.time), tzinfo)
@@ -37,14 +56,14 @@ def git_show(commit):
         ]
     )
     print(msg)
-    show_stat(commit)
+    show_stat(repo, commit)
 
 
 def git_log(repo, number_of_commits):
     last = repo[repo.head.target]
     for commit in repo.walk(last.id, pygit2.enums.SortMode.TIME):
         # print(commit.short_id, ':', commit.message.strip())
-        git_show(commit)
+        git_show(repo, commit)
         print()
         number_of_commits -= 1
         if number_of_commits <= 0:
@@ -52,6 +71,8 @@ def git_log(repo, number_of_commits):
 
 
 if __name__ == "__main__":
-    repository_path = pygit2.discover_repository(sys.argv[1])
-    repo = pygit2.Repository(repository_path)
+    repository_path = sys.argv[1]
+    repository_git_path = pygit2.discover_repository(repository_path)
+    repo = pygit2.Repository(repository_git_path)
     git_log(repo, 3)
+    show_readme(repository_path)
